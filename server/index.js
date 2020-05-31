6b0a699191669d46335c6f883242c0e6b985c7e0
StaxGit <lario.free@gmail.com>
2020-05-31 10:10:30 +0200

    Add error handler

--- server/index.js
+++ server/index.js
@@ -3,14 +3,24 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
+const errorHandler = require("./handlers/error.js");
+
 app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", function(req, res, next) {
-  res.send("Connected");
-})
+  res.json("Connected");
+});
+
+app.use(function (req, res, next) {
+  const err = new Error("Not Found!");
+  err.status = 404;
+  next(err);
+});
+
+app.use(errorHandler);
 
 const port = process.env.PORT || 3001;
 app.listen(port, function () {
   console.log(`Server listening on port ${port}!`)
-})
+});
