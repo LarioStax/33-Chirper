183bf481cd793fd91b57600a70e46f52d3ff6421
StaxGit <lario.free@gmail.com>
2020-05-31 11:13:38 +0200

    Install, require and config dotenv

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const dotenv = require("dotenv").config();
 
 const errorHandler = require("./handlers/error.js");
 
