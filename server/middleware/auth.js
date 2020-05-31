aed0b21906c6450160247c72112476e8bb4f9a2b
StaxGit <lario.free@gmail.com>
2020-05-31 13:43:24 +0200

    Add middleware to check if user logged in

---
+++ server/middleware/auth.js
@@ -0,0 +1,26 @@
+const dotenv = require("dotenv").load();
+const jwt = require("jsonwebtoken");
+
+//make sure the user is logged in - Authentication
+exports.loginRequired = function (req, res, next) {
+  try {
+    const token = req.headers.authorization.split(" ")[1] // Bearer dsjklfhadf
+    jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
+      if (decoded) {
+        return next();
+      } else {
+        return next({
+          status: 401,
+          message: "Please log in first"
+        })
+      }
+    })
+  } catch (err) {
+    return next({
+      status: 401,
+      message: "Please log in first"
+    })
+  }
+};
+
+//makse sure we got the correct user - Authorization
