33ced996ef431623a4ae648ff8ac257a18774553
StaxGit <lario.free@gmail.com>
2020-05-31 12:34:02 +0200

    Handle login and api/auth/login post route

--- server/handlers/auth.js
+++ server/handlers/auth.js
@@ -1,7 +1,44 @@
 const db = require("../models/index.js");
 const jwt = require("jsonwebtoken");
 
-exports.login = function () {
+exports.login = async function (req, res, next) {
+  try {
+    //finding a user
+    let user = await db.User.findOne({
+      email: req.body.email
+    });
+    let { id, username, profileImageUrl } = user;
+    //check if their password matches the saved one
+    let isMatch = await user.comparePassword(req.body.password);
+    //if it all matches
+    if (isMatch) {
+      let token = jwt.sign({
+        id,
+        username,
+        profileImageUrl
+      },
+        process.env.SECRET_KEY
+      );
+      //log them in
+      return res.status(200).json({
+        id,
+        username,
+        profileImageUrl,
+        token
+      })
+    } else {
+      return next({
+        status: 400,
+        message: "Invalid Email/Password!"
+      })
+    }
+  } catch (err) {
+    return next({
+      status: 400,
+      message: "Invalid Email/Password!"
+    })
+  }
+
 
 }
 
@@ -23,7 +60,7 @@ exports.signup = async function (req, res, next) {
     })
     //create a token (signing a token)
     //process.env.SECRET_KEY
-  } catch (err) {    
+  } catch (err) {
     if (err.code === 11000) {
       err.message = "Sorry, that username and/or email is taken!";
     }
