b73b5ec9cc6189d33e9b876f08f9a3a249545e1d
StaxGit <lario.free@gmail.com>
2020-06-01 11:01:37 +0200

    Create LogIn and SignUp forms

--- client/src/containers/Main.js
+++ client/src/containers/Main.js
@@ -1,20 +1,32 @@
 import React from "react";
 import { Switch, Route, withRouter, Redirect } from "react-router-dom";
-import {connect} from "react-redux";
-import Homepage from "../components/Homepage";
+import { connect } from "react-redux";
+
+import Homepage from "../components/Homepage.js";
+import AuthForm from "../components/AuthForm.js"
 
 const Main = props => {
   return (
     <div className="container">
       <Switch>
-        <Route
-          exact path="/"
-          render={
-            (props) => (<Homepage {...props} />)
-          }
-        >
-
-        </Route>
+        <Route exact path="/" render={(props) => (
+          <Homepage {...props} />
+        )} />
+        <Route exact path="/login" render={(props) => (
+          <AuthForm
+            buttonText="Log in"
+            heading="Welcome back!"
+            {...props}
+          />
+        )} />
+        <Route exact path="/signup" render={(props) => (
+          <AuthForm
+            buttonText="Sign me up!"
+            heading="Join Chirper today!"
+            signUp
+            {...props}
+          />
+        )} />
       </Switch>
     </div>
   )
