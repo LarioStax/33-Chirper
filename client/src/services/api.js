fbfc5e873cc0cb6174dd085cacbbeabfd2ab3e0b
StaxGit <lario.free@gmail.com>
2020-06-02 09:44:10 +0200

    Fix typo

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,7 +4,7 @@ export function setTokenHeader(token) {
   if(token) {
     axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
   } else {
-    delete axios.defaults.headers.commong["Authorization"];
+    delete axios.defaults.headers.common["Authorization"];
   }
 }
 
