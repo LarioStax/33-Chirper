a05846368b2b6769c0272760240712a644c4f419
StaxGit <lario.free@gmail.com>
2020-06-03 07:48:32 +0200

    Add delete functionality and show delete button when authorized

--- client/src/containers/MessageList.js
+++ client/src/containers/MessageList.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { getMessages } from "../store/actions/messages.js";
+import { getMessages, removeMessage } from "../store/actions/messages.js";
 import MessageItem from "../components/MessageItem.js";
 import "./MessageList.css";
 
@@ -9,7 +9,7 @@ class MessageList extends Component {
     this.props.getMessages();
   }
   render() {
-    const { messages } = this.props;
+    const { messages, removeMessage, currentUser } = this.props;
     let messageList = messages.map(m => (
       <MessageItem
         key={m._id}
@@ -17,24 +17,27 @@ class MessageList extends Component {
         text={m.text}
         username={m.user.username}
         profileImageUrl={m.user.profileImageUrl}
+        removeMessage={removeMessage.bind(this, m.user._id, m._id)}
+        isCorrectUser={currentUser === m.user._id}
       />
     ))
     return (
       <div className="row col-sm-8">
         <div className="offset-1 col-sm-10">
-         <ul className="list-group" id="messages">
-          {messageList}
-         </ul>
+          <ul className="list-group" id="messages">
+            {messageList}
+          </ul>
         </div>
-      </div>      
+      </div>
     )
   }
 }
 
 function mapStateToProps(state) {
   return {
-    messages: state.messages
+    messages: state.messages,
+    currentUser: state.currentUser.user.id
   }
 }
 
-export default connect(mapStateToProps, { getMessages })(MessageList);
+export default connect(mapStateToProps, { getMessages, removeMessage })(MessageList);
--- client/src/store/actions/messages.js
+++ client/src/store/actions/messages.js
@@ -15,6 +15,19 @@ export const getMessages = () => {
   }
 }
 
+export const remove = (id) => ({
+  type: REMOVE_MESSAGE,
+  id
+});
+
+export const removeMessage = (user_id, message_id) => {
+  return dispatch => {
+    return apiCall("delete", `/api/users/${user_id}/messages/${message_id}`)
+      .then(() => dispatch(remove(message_id)))
+      .catch((err) => dispatch(addError(err.message)))
+  }
+}
+
 export const postNewMessage = text => (dispatch, getState) => {
   let { currentUser } = getState();
   const id = currentUser.user.id;
